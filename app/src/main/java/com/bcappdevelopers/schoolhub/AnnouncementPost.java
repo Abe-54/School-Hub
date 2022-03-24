387ad63c8968d11c9bb0571d6e361c3a51ec6aa0
Saxplayer790 <63307242+Saxplayer790@users.noreply.github.com>
2022-03-24 11:26:24 -0400

    Added Basic layout and activities for examples

---
+++ app/src/main/java/com/bcappdevelopers/schoolhub/AnnouncementPost.java
@@ -0,0 +1,14 @@
+package com.bcappdevelopers.schoolhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnnouncementPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_announcement_post);
+    }
+}
