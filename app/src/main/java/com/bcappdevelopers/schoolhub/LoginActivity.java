ea0db0d95e1ca292cbfc715ead9903afb2261b24
Abraham Rubio <abraham_rubio@bloomfield.edu>
2022-03-30 14:36:36 -0400

    Added functionality to buttons on the login screen The user can click the signup button and be directed to the sign up screen The user can leave the signup screen by clicking the text saying they already have an account I also changed the names of certain components

--- app/src/main/java/com/bcappdevelopers/schoolhub/LoginActivity.java
+++ app/src/main/java/com/bcappdevelopers/schoolhub/LoginActivity.java
@@ -18,23 +18,32 @@ import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //TAG FOR DEBUGGING
     public static final String TAG = "Login";
+
+    //SCREEN COMPONENTS VARIABLES
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //automatically login the user if it exists
         if(ParseUser.getCurrentUser() != null){
             goToHome();
         }
 
+        //assigns components to actual components
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignUp_loginActivity);
+
+        //click listener for login button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +51,15 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username,password);
+            }
+        });
+
+        //click listener for signup button
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"onClickSignupButton");
+                gotToSignup();
             }
         });
     }
@@ -66,6 +84,7 @@ public class LoginActivity extends AppCompatActivity {
     private void goToHome() {
         Intent i;
 
+        //Decides which side of the app the user goes to
         if(ParseUser.getCurrentUser().getBoolean("isAdmin")) {
             i = new Intent(this, AdminHomeActivity.class);
         } else {
@@ -74,6 +93,12 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(i);
         finish();
 
-        }
     }
+
+    private void gotToSignup() {
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
 
--- app/src/main/java/com/bcappdevelopers/schoolhub/SignUpActivity.java
+++ app/src/main/java/com/bcappdevelopers/schoolhub/SignUpActivity.java
@@ -1,4 +1,78 @@
 package com.bcappdevelopers.schoolhub;
 
-public class SignUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    //TAG FOR DEBUGGING
+    public static final String TAG = "Signup";
+
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+    private Button btnSignUp;
+    private CheckBox cbConfirmation;
+    private TextView tvAlreadyMember;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_signup);
+
+        etUsername = findViewById(R.id.etUsernameField);
+        etEmail = findViewById(R.id.etEmailField);
+        etPassword = findViewById(R.id.etPasswordField);
+        etConfirmPassword = findViewById(R.id.etPasswordFieldVerify);
+        btnSignUp = findViewById(R.id.btnSignup_signupActivity);
+        cbConfirmation = findViewById(R.id.cbTermsAgreement);
+        tvAlreadyMember = findViewById(R.id.tvAlreadyMemeberLink);
+
+//        click listener for button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"onClickLoginButton");
+
+                String username = etUsername.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                Boolean acceptedConditions = cbConfirmation.isActivated();
+
+
+                signupStudent(username, email, password, acceptedConditions);
+            }
+        });
+
+        tvAlreadyMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotToLogin();
+            }
+        });
+    }
+
+    private void signupStudent(String username, String email, String password, Boolean confirmation){
+        //TODO: finish method
+        //TODO: post to back4app and login student
+
+        Toast.makeText(this, "SIGNED UP", Toast.LENGTH_SHORT).show();
+    }
+
+    private void gotToLogin() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
 }
